73b26f096b6ebd5f5e82be8a8a4e24859c3bf752
Daniel Kim <danielynskim@gmail.com>
2021-08-04 22:18:12 -0400

    Add more functionality to notif bot

--- cog.py
+++ cog.py
@@ -12,6 +12,10 @@ class RedditCog(commands.Cog):
         user_agent = "NotifBot:v1.0 (by u/CobaltGray0)"
         self.reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
+        self.running = False
+        self.sub = None
+        self.match = none
+
     @commands.command(name="ping")
     @guild_only()
     async def ping(self, ctx):
@@ -20,7 +24,29 @@ class RedditCog(commands.Cog):
     @commands.command(name="notif")
     @guild_only()
     async def notif(self, ctx, sub: str, match: str):
+        if self.running:
+            await ctx.send("Notif bot already running!")
+            return
+
+        self.running = True
+        self.sub = sub
+        self.match = match
+
         subreddit = await self.reddit.subreddit(sub)
         async for submission in subreddit.stream.submissions():
-            if match in submission.selftext.lower():
-               await ctx.send(f"{ctx.author.mention}: new link found!\n{submission.permalink}")
+            if self.running:
+                if match in submission.selftext.lower():
+                    await ctx.send(f"{ctx.author.mention}: new link found!\nwww.reddit.com{submission.permalink}")
+
+    async def notif_shutdown(self, ctx):
+        if not self.running:
+            await ctx.send("Notif bot is not currently running!")
+        else:
+            self.running = False
+            await ctx.send("Notif bot shutdown successfully.")
+
+    async def notif_info(self, ctx):
+        if self.running:
+            await ctx.send(f"Notif bot currently running. Sub:{self.sub} Match:{self.match}")
+        else:
+            await ctx.send("Notif bot not running!")
